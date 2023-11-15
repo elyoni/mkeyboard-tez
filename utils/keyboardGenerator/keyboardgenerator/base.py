332d66b2dc890e3002205624f55dc6ecd47a4607
Yehonatan Elentok <yehonatan.e@solaredge.com>
2023-11-15 09:39:16 +0200

    [Backup]

--- utils/keyboardGenerator/keyboardgenerator/base.py
+++ utils/keyboardGenerator/keyboardgenerator/base.py
@@ -16,6 +16,14 @@ class Point:
     def get_tuple(self) -> tuple[float, float]:
         return (self.x, self.y)
 
+    def calc_rotate_xy(self, center_rotation_point: "Point", rotation_degree: float):
+        angle = np.deg2rad(rotation_degree)
+        R = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+        np_center = np.atleast_2d((center_rotation_point.x, center_rotation_point.y))
+        np_point = np.atleast_2d((self.x, self.y))
+        np_point = np.squeeze((R @ (np_point.T - np_center.T) + np_center.T).T)
+        return Point.from_np(np_point)
+
     @classmethod
     def from_np(cls, np: np.ndarray):
         return cls(float(np[0]), float(np[1]))
@@ -73,14 +81,20 @@ class Point:
 
 # General Keyboard part, can be a key, mcu, etc.
 class Part(ABC):
-    footprint_pcb: tuple[float, float, float]  # Footprint Size the part on the pcb
-    footprint_case: tuple[float, float, float]  # Footprint Size the part on the pcb
+    footprint_pcb: tuple[
+        float, float, float
+    ]  # Footprint Size the part on the pcb in mm
+    footprint_case: tuple[
+        float, float, float
+    ]  # Footprint Size the part on the pcb in mm
 
-    pos: Point  # Center Point of the part
-    rotate_degree: float  # I
+    pos: Point  # Center Point of the part in mm NOT u.
+    rotation_degree: float  # Equle 0 If no need to rotate the part
+    border_pcb: float  # Add additional border to the part
 
     @abstractmethod
     def draw(self) -> OpenSCADObject:
+        # First need to calculate the rotation, if has and then draw
         pass
 
     @abstractmethod
@@ -91,11 +105,3 @@ class Part(ABC):
     def from_unit_to_mm(self, float) -> float:
         # Convert KLE units into real number
         pass
-
-    def calc_rotate_xy(self, center_rotation_point: Point) -> Point:
-        angle = np.deg2rad(self.rotate_degree)
-        R = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-        np_center = np.atleast_2d(center_rotation_point.get_tuple())
-        np_point = np.atleast_2d(self.pos.get_tuple())
-        np_point = np.squeeze((R @ (np_point.T - np_center.T) + np_center.T).T)
-        return Point.from_np(np_point)
--- utils/keyboardGenerator/keyboardgenerator/key.py
+++ utils/keyboardGenerator/keyboardgenerator/key.py
@@ -1,95 +1,13 @@
+#!/usr/bin/env python3
+
 from keyboardgenerator.base import Point, Part
 
 
 class Key(Part):
-    pos: Point
-    pos_spacing: Point
-    corners: tuple[Point, Point, Point, Point]
-    corners_spacing: tuple[Point, Point, Point, Point]
-    rotation_angle: float  # Degrees
-    center_rotation_point: Point
-    spacing: Point
-    height: float
-    width: float
-    key_boarder: float  # in key units
+    key_spacing: float = 0
+    pass
 
-    def __init__(self, key: kle_serial.Key, key_boarder: float = 0) -> None:
-        self.spacing = Point(
-            MX_KEY_SPACING,  # In the future need to collect this data from the Json file
-            MX_KEY_SPACING,
-        )
-        self.key_boarder = key_boarder
-        self.key_origin = key
-        self.pos = Point(key.x, key.y)
-        self.center_rotation_point = Point(key.rotation_x, key.rotation_y)
-        self.rotation_angle = key.rotation_angle
+    # def __init__(self, key: kle_serial.Key, key_boarder: float = 0) -> None:
+        # pass
 
-        # Set to the center of the key
-        if key.width > 0 or key.height > 0:
-            self.pos.x += key.width / 2
-            self.pos.y += key.height / 2
-
-        self.height = key.height
-        self.width = key.width
-
-        self.calcuate_corners(key_boarder)
-
-        # Calculate the center
-        if key.rotation_angle != 0:
-            self.pos = self.calc_rotate_xy(self.pos, self.center_rotation_point)
-        self.pos_spacing = self.pos * self.spacing
-
-    def draw_key_hold(self) -> OpenSCADObject:
-        try:
-            return (
-                cube(self.spacing.x, self.spacing.y, 8, center=True)
-                .rotate(-self.rotation_angle)
-                .translate(self.pos_spacing.x, -self.pos_spacing.y)
-                .down(1)
-            )
-        except Exception:
-            print(type(self.pos_spacing.x), type(self.pos_spacing.y))
-            raise
-
-    def calcuate_corners(self, key_boarder):
-        self.corners = (
-            Point(
-                self.pos.x - (self.width / 2 + key_boarder),
-                self.pos.y + (self.height / 2 + key_boarder),
-            ),
-            Point(
-                self.pos.x - (self.width / 2 + key_boarder),
-                self.pos.y - (self.height / 2 + key_boarder),
-            ),
-            Point(
-                self.pos.x + (self.width / 2 + key_boarder),
-                self.pos.y + (self.height / 2 + key_boarder),
-            ),
-            Point(
-                self.pos.x + (self.width / 2 + key_boarder),
-                self.pos.y - (self.height / 2 + key_boarder),
-            ),
-        )
-
-        self.corners_spacing = (
-            self.corners[0] * self.spacing,
-            self.corners[1] * self.spacing,
-            self.corners[2] * self.spacing,
-            self.corners[3] * self.spacing,
-        )
-
-        if self.rotation_angle != 0:
-            self.corners_spacing = (
-                self.calc_rotate_xy(
-                    self.corners_spacing[0], self.center_rotation_point * self.spacing
-                ),
-                self.calc_rotate_xy(
-                    self.corners_spacing[1], self.center_rotation_point * self.spacing
-                ),
-                self.calc_rotate_xy(
-                    self.corners_spacing[2], self.center_rotation_point * self.spacing
-                ),
-                self.calc_rotate_xy(
-                    self.corners_spacing[3], self.center_rotation_point * self.spacing
-                ),
-            )
+    def __init__(self, position: Point, rotation_degree: float, rotation_center: Point, border: Point
